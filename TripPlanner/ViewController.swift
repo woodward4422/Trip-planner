9ee28256a79f1efb30c39f25e23fd34355504e67
noah <noahwoodward2244@gmail.com>
2019-04-26 08:11:01 -0700

    First commit

---
+++ TripPlanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TripPlanner
+//
+//  Created by Noah Woodward on 4/26/19.
+//  Copyright © 2019 Noah Woodward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
